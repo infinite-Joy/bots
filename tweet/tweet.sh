1728133cccf0408058819c2e7d3b89cc7f95ee8d
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2016-11-17 14:38:27 +0530

    automate the whole process using shell

---
+++ tweet/tweet.sh
@@ -0,0 +1,37 @@
+cd bots
+source venvpy35/bin/activate
+cd tweet
+
+python get_quotes.py > programming_quote.txt
+
+echo "tweet procured"
+
+cat programming_quote.txt
+quote=$(cat programming_quote.txt)
+
+cd ../..
+
+cd bayesian-classifier
+
+deactivate
+
+python bayes.py classify ../bots/tweet/programming_quote.txt programming non_programming > programming_index.txt
+
+echo "classification done"
+
+cat programming_index.txt
+index=$(cat programming_index.txt | grep -o '[0-9][0-9]')
+
+echo "index is ${index}"
+
+cd ..
+
+if [ "${index}" -gt 20 ]; then
+	cd bots
+	source venvpy35/bin/activate
+	cd tweet
+	python tweet.py "${quote}"
+
+fi
+
+
